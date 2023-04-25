7550c7d12a7df99306dfb1fb58559c954c820646
yingtao <yt1577396227@163.com>
2023-04-25 18:25:04 +0800

    feat:pb结构变更

--- order/src/main/java/com/zufe/yt/order/interfaces/facade/SeckillOrderGrpc.java
+++ order/src/main/java/com/zufe/yt/order/interfaces/facade/SeckillOrderGrpc.java
@@ -1,11 +1,22 @@
 package com.zufe.yt.order.interfaces.facade;
 
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+import order.SeckillOrderRpc;
+import order.SeckillOrderServiceGrpc;
+
 /**
  * @author yt
  * @package: com.zufe.yt.order.interfaces.facade
  * @className: SeckillOrderGrpc
  * @date 2023/4/24
- * @description: 订单接口层
+ * @description: 订单接口层,只做写订单用，因为压力最大，查询订单放到用户领域去做
  */
-public class SeckillOrderGrpc {
+
+@GrpcService
+public class SeckillOrderGrpc extends SeckillOrderServiceGrpc.SeckillOrderServiceImplBase {
+    @Override
+    public void addOrder(SeckillOrderRpc.OrderMessage.AddOrderReq request, StreamObserver<SeckillOrderRpc.OrderMessage.CommonRely> responseObserver) {
+        super.addOrder(request, responseObserver);
+    }
 }
